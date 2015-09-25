2d7fe44373cd8dad2f391e36f7543814d5db77e6
mthpvg <mth.pvg@gmail.com>
2015-09-25 15:34:52 +0200

    node.js install updated to 4.x

--- install_nodeJS.sh
+++ install_nodeJS.sh
@@ -8,7 +8,8 @@ sudo apt-get install -y gcc g++ make build-essential
 
 # Installing node
 # https://github.com/nodesource/distributions
-curl -sL https://deb.nodesource.com/setup_$nodeVersion | sudo -E bash -
+#http://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 #sudo ln -s /usr/bin/nodejs /usr/bin/node
 
