0b417b021e4fb14051f4c71174521592707097f8
mthpvg <mth.pvg@gmail.com>
2016-02-19 17:51:56 +0100

    nginx latest and install node with nvm

--- add_nginX_vhost.sh
+++ add_nginX_vhost.sh
@@ -4,8 +4,9 @@
 #$2 is port
 
 cd nginx
-sed -e "s/SERVERNAME/$1/g" -e "s/PORT/$2/g" sub.domain.tld > $1
 
-sudo mv $1 /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
+sed -e "s/SERVERNAME/$1/g" -e "s/PORT/$2/g" sub.domain.tld > $1.conf
+
+sudo mv $1 /etc/nginx/conf.d
+
 sudo service nginx restart
--- install_nodeJS.sh
+++ install_nodeJS.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Parameters
-#nodeVersion='4.2.2'
-
-# Prerequisites
+# PREREQUISITES
 sudo apt-get update
-sudo apt-get install -y gcc g++ make build-essential
-sudo apt-get install libkrb5-dev
+sudo apt-get install -y gcc g++ make build-essential libssl-dev libkrb5-dev
 
-# Installing node
-# https://github.com/nodesource/distributions
-#http://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs
-#sudo ln -s /usr/bin/nodejs /usr/bin/node
+# NVM
+# https://github.com/creationix/nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+source ~/.bashrc
+source  ~/.bash_profile
 
-# Installing forever
-sudo npm install -g forever
+#NODE
+nvm install stable
+
+#UPDATE NPM
+npm install -g npm
+
+# FOREVER
+npm install -g forever
